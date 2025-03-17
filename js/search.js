4740d5a8e3fdef7a72b80f9a5044d89b8b7fdb18
6wensitan <pch124678@163.com>
2025-03-17 19:43:56 +0800

    Site updated: 2025-03-17 19:43:55

--- js/search.js
+++ js/search.js
@@ -1,74 +1,32 @@
-(() => {
-    var searchIndex = []
-    const searchForm = document.getElementById('search-form')
-    const searchBox = document.getElementById('searchbox')
-    const searchResults = document.getElementById('search-results')
+(function ($) {
+  let $searchWrap = $('#search-form-wrap'),
+    isSearchAnim = false,
+    searchAnimDuration = 200;
 
-    searchBox.select()
+  function startSearchAnim() {
+    isSearchAnim = true;
+  }
 
-    const doSearch = (keyword) => {
-        var results = []
-        var resultsEl = []
+  function stopSearchAnim(callback) {
+    setTimeout(function () {
+      isSearchAnim = false;
+      callback && callback();
+    }, searchAnimDuration);
+  }
 
-        for (let i = 0; i < searchIndex.length; i++) {
-            const currentItem = searchIndex[i];
+  $('#nav-search-btn').on('click', function () {
+    if (isSearchAnim) return;
 
-            if (JSON.stringify(currentItem).search(keyword) !== -1) {
-                results.push(currentItem)
-            }
-        }
+    startSearchAnim();
+    $searchWrap.addClass('on');
+    stopSearchAnim(function () {
+      $('.search-form-input').focus();
+    });
+  });
 
-        if (results.length > 0) {
-            for (let i = 0; i < results.length; i++) {
-                const currentResult = results[i];
-                const currentResultEl = document.createElement('article')
-                currentResultEl.classList.add('post-list-item')
-                currentResultEl.innerHTML = `
-<a href="${currentResult.url}">
-    <div class="content">
-        ${(() => {
-                        if (currentResult.categories) {
-                            return `<div class="categories${document.body.attributes['data-uppercase-categories'].value ? ' text-uppercase' : ''}">${(() => {
-                                let categories = ''
-                                for (let i = 0; i < currentResult.categories.length; i++) {
-                                    const currentCategory = currentResult.categories[i]
-                                    categories += `<span>${currentCategory}</span>`
-                                }
-                                return categories
-                            })()}</div>`
-                        } else {
-                            return ''
-                        }
-                    })()}
-        <div class="title">${currentResult.title}</div>
-    </div>
-</a>
-                `
-                resultsEl.push(currentResultEl)
-            }
-        } else {
-            const el = document.createElement('div')
-            el.className = 'no-results'
-            el.innerHTML = 'No results found.'
-            resultsEl.push(el)
-        }
-
-        searchResults.innerHTML = ''
-        for (let i = 0; i < resultsEl.length; i++) {
-            const element = resultsEl[i];
-            searchResults.appendChild(element)
-        }
-    }
-
-    searchForm.addEventListener('submit', (ev) => {
-        ev.preventDefault()
-        if (searchIndex != '') {
-            doSearch(searchBox.value)
-        } else {
-            fetch(document.body.attributes['data-config-root'].value + document.body.attributes['data-search-path'].value).then(res => res.json()).then(data => {
-                searchIndex = data
-                doSearch(searchBox.value)
-            })
-        }
-    })
-})()
+  $('.search-form-input').on('blur', function () {
+    startSearchAnim();
+    $searchWrap.removeClass('on');
+    stopSearchAnim();
+  });
+})(jQuery);
